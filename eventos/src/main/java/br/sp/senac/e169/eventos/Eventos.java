e4a6f4d4c4fd3f6573ca49c2c465951a02be4607
AleRomao <129014981+AleRomao@users.noreply.github.com>
2023-09-29 20:25:12 -0300

    Add files via upload

---
+++ eventos/src/main/java/br/sp/senac/e169/eventos/Eventos.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package br.sp.senac.e169.eventos;
+
+/**
+ *
+ * @author alexandre.romao
+ */
+public class Eventos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ eventos/src/main/java/br/sp/senac/e169/eventos/keyEvent.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.sp.senac.e169.eventos;
+
+/**
+ *
+ * @author alexandre.romao
+ */
+class keyEvent {
+
+    static char VK_BACK_SPA;
+    
+}
