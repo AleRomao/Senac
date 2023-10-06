63d9a568371eb3744b6ef4f3dc11e2f07b9c2980
AleRomao <129014981+AleRomao@users.noreply.github.com>
2023-10-06 00:00:23 -0300

    Add files via upload

---
+++ pontodevenda/src/main/java/br/sp/senac/e169/pontodevenda/Pontodevenda.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.sp.senac.e169.pontodevenda;
+
+/**
+ *
+ * @author romao
+ */
+public class Pontodevenda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
